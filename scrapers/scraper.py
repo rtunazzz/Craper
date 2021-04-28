47dbe93d7a551f625af126a556258ed34ab53103
Artur Hnát <hnatartu@fit.cvut.cz>
2021-04-28 09:56:03 +0200

    [SCRAPER] - Add DB initialization

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -1,14 +1,16 @@
-from scrapers.models.snipes import Snipes
-from scrapers.db.db import DatabaseWrapper
+from models import Footpatrol, Size, JDSports, TheHipStore,  Solebox, Snipes, Onygo
+from db.db import DatabaseWrapper
+from pathlib import Path
+from os import path, makedirs
 
 SITES = {
-    'footpatrol': Snipes,
-    'size': Snipes,
-    'jdsports': Snipes,
-    'thehipstore': Snipes,
-    'solebox': Snipes,
+    'footpatrol': Footpatrol,
+    'size': Size,
+    'jdsports': JDSports,
+    'thehipstore': TheHipStore,
+    'solebox': Solebox,
     'snipes': Snipes,
-    'onygo': Snipes,
+    'onygo': Onygo,
 }
 
 class Scraper:
@@ -18,6 +20,15 @@ class Scraper:
             raise ValueError(f"Scaper for '{site_name}' is not supported.")
         
         self.name = site_name
-        
         self.site = SITES[site_name.lower()]
-        self.db = DatabaseWrapper(f"./data/{site_name}.db")
+
+        self._absolute_path = Path(__file__).parent.absolute()
+        data_folder_path = f"{self._absolute_path}/data"
+        if not path.exists(data_folder_path):
+            makedirs(data_folder_path)       
+ 
+        self.db = DatabaseWrapper(f"{data_folder_path}/{site_name}.db")
+
+if __name__ == '__main__':
+    s = Scraper('footpatrol')
+    print(s.name)
