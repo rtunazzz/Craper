0d7fe988a80238fdf410fe7079e0de9aba6fc0b8
Artur Hnát <hnatartu@fit.cvut.cz>
2021-05-12 20:07:48 +0200

    [GENERAL] - Optimize imports

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 #!/usr/local/bin/python3
 
-import argparse
+from argparse import ArgumentParser
 from scrapers import Scraper
 from sys import exit
 
-parser = argparse.ArgumentParser(description='Sneaker scraper launcher')
+parser = ArgumentParser(description='Sneaker scraper launcher')
 
 parser.add_argument('site', metavar='<SITE_NAME>', type=str, help='Name of the site to scrape')
 
-parser.add_argument('-t', '--nthreads', metavar='<num>', type=int, default=100, help='Number of threads to run')
-parser.add_argument('-n', '--perthread', metavar='<num>', type=int, help='Number of PIDs per thread')
+parser.add_argument('-t', '--nthreads', metavar='<number>', type=int, default=10, help='Number of threads to run')
+parser.add_argument('-n', '--perthread', metavar='<number>', type=int, help='Number of PIDs per thread')
 parser.add_argument('-s', '--start', metavar='<pid>', type=str, default='1', help='Product ID to start from')
 parser.add_argument('-e', '--end', metavar='<pid>', type=str, default='-1', help='Product ID to end at')
+parser.add_argument('-d', '--delay', metavar='<number>', type=int, default=1, help='Delay which slows down the script (to prevent bans)')
 parser.add_argument('-p', '--proxies', action='store_true', help='Use proxies')
 parser.add_argument('--debug', action='store_true', help='Turns on debugging mode, logs more info.')
 
@@ -24,6 +25,7 @@ try:
         stop_pid=args.end,
         use_proxies=args.proxies,
         debug=args.debug,
+        delay=args.delay,
     )
 
     if args.perthread is not None:
